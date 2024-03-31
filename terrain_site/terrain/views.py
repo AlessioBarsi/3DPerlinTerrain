17fdd37de5f23e72fb557e103797d7f4571db72b
AlessioBarsi <160592653+AlessioBarsi@users.noreply.github.com>
2024-03-31 21:46:36 +0200

    Added custom amount of vertex sample generation

--- terrain_site/terrain/views.py
+++ terrain_site/terrain/views.py
@@ -8,9 +8,10 @@ def index(request):
 
 def generator(request):
 
-    #Generate a sample of 9 vertices with perlin noise
-    """
-    #Delete old vertices
+    #It should be a perfect cube
+    VERTEX_COUNT = 81
+    #Generate a sample of vertices with perlin noise
+    #Delete old vertices first
     vertices = Vertex.objects.all().values()    
     for v in Vertex.objects.all():
         v.delete()
@@ -18,10 +19,10 @@ def generator(request):
     if len(vertices) == 0:
         from .functions import getPerlinNoise
         noise = getPerlinNoise(10, 1)
-        for j in range(3):
-            for i in range(3):
+        import math
+        for j in range(int(math.sqrt(VERTEX_COUNT))):
+            for i in range(int(math.sqrt(VERTEX_COUNT))):
                 newVertex = Vertex.objects.create(x_coord = 1+i, y_coord = 1+j, z_coord = noise( ((i*j)+1)/100 ) )
-    """
 
     context = {
         'vertices' : Vertex.objects.all().values(),
