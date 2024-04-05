80891569f31ce544f14bbc78846156d22c76f3f1
AlessioBarsi <alessiobarsi98@gmail.com>
2024-04-05 10:42:20 +0200

    Added checkbox for enabling/disabling Perlin Noise in terrain generation

--- terrain_site/terrain/views.py
+++ terrain_site/terrain/views.py
@@ -13,17 +13,26 @@ def generator(request):
     #Get perlin_noise and terrain settings from html form
     SCALE = 0.1
     OCTAVES = 30
-    VERTEX_COUNT = 1000
+    VERTEX_COUNT = 100
     HEIGHT_CURVE = 'Linear'
     MAX_HEIGHT = 1
+    PERLIN_NOISE_ENABLED = True
 
     if request.method == 'POST':
         form = request.POST
-        SCALE = float(form['input_scale'])
         VERTEX_COUNT = int(form['input_terrain_size'])
-        OCTAVES = float(form['input_octaves'])
         HEIGHT_CURVE = form['height_curve']
+
+        #Check if the Perlin Noise has been enabled in the form
         
+        if (request.POST.get('perlin_noise_checkbox') == 'on'):
+            PERLIN_NOISE_ENABLED = True
+        else:
+            PERLIN_NOISE_ENABLED = False
+        if (PERLIN_NOISE_ENABLED):
+            OCTAVES = float(form['input_octaves'])
+            SCALE = float(form['input_scale'])
+
     #Delete existing vertices first
     vertices = Vertex.objects.all().values()    
     for v in Vertex.objects.all():
@@ -31,17 +40,21 @@ def generator(request):
     #Generate a set of new vertices
     if len(vertices) == 0:
         #Generate the perlin noise object with set octaves and seed
-        from .functions import getPerlinNoise
-        noise = getPerlinNoise(OCTAVES, 1)
         from .functions import getHeightFunction
 
         for x in range(int(math.sqrt(VERTEX_COUNT))):
             for y in range(int(math.sqrt(VERTEX_COUNT))):
                 #Z coordinate is defined by perlin_noise + eventual function to shape the terrain height
+                #Generate Perlin Noise value if checkbox was enabled
+                z_value = 1
+                if (PERLIN_NOISE_ENABLED):
+                    from .functions import getPerlinNoise
+                    noise = getPerlinNoise(OCTAVES, 1)
+                    z_value = noise([x / math.sqrt(VERTEX_COUNT) * SCALE, y / math.sqrt(VERTEX_COUNT) * SCALE])
                 newVertex = Vertex.objects.create(x_coord = 1+y,
-                                                y_coord = 1+x,
-                                                z_coord = noise([x / math.sqrt(VERTEX_COUNT) * SCALE, y / math.sqrt(VERTEX_COUNT) * SCALE])
-                                                                + getHeightFunction(HEIGHT_CURVE.lower(), x, y, math.sqrt(VERTEX_COUNT), MAX_HEIGHT) )
+                                                  y_coord = 1+x,
+                                                  z_coord = z_value
+                                                          + getHeightFunction(HEIGHT_CURVE.lower(), x, y, math.sqrt(VERTEX_COUNT), MAX_HEIGHT) )
 
     context = {
         'vertices' : Vertex.objects.all().values(),
